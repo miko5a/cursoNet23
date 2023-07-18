96464f2d5234ea8dfbaea94fb09dc47a054febe7
Alberto de León <alberto13590@hotmail.com>
2023-07-18 13:02:25 +0200

    Commit Inical

---
+++ PracticaCochesSolucion/Entidades/Vehiculos.cs
@@ -0,0 +1,65 @@
+﻿namespace Entidades
+{
+    public class Vehiculos
+    {
+        public string Matricula { get; set; }
+        private double ValorBase { get; set; }
+        private double PorcentajeEtiqueta { get; set; }
+        public DateTime FechaMatriculacion { get; set; } = DateTime.Now;
+
+        public enum Etiqueta
+        {
+            sinEtiqueta,
+            etiquetaB,
+            etiquetaC,
+            etiquetaECO,
+            etiquetaCERO
+        }
+        private Etiqueta _etiqueta;
+
+        public Vehiculos(string matricula, DateTime fechaMatriculacion, Etiqueta etiqueta)
+        {
+            Matricula = matricula;
+            FechaMatriculacion = fechaMatriculacion;
+            _etiqueta = etiqueta;
+            ValorBase = 1000;
+        }
+
+        public void FijarEtiqueta(Etiqueta etiqueta)
+        {
+            this._etiqueta = etiqueta;
+        }
+
+        public void CalcularImpuesto()
+        {
+            switch (_etiqueta)
+            {
+                case Etiqueta.sinEtiqueta:
+                    PorcentajeEtiqueta = 25;
+                    break;
+                case Etiqueta.etiquetaB:
+                    PorcentajeEtiqueta = 15;
+                    break;
+                case Etiqueta.etiquetaC:
+                    PorcentajeEtiqueta = 10;
+                    break;
+                case Etiqueta.etiquetaCERO:
+                    PorcentajeEtiqueta = 0;
+                    break;
+                case Etiqueta.etiquetaECO:
+                    PorcentajeEtiqueta = 5;
+                    break;
+            }
+
+            var añoAntiguedad = DateTime.Now.Year - FechaMatriculacion.Year;
+
+            var sumaPorcentajes = añoAntiguedad + PorcentajeEtiqueta;
+
+            var cantidadImpuesto = ((sumaPorcentajes / 100) * ValorBase) + ValorBase; 
+
+            Console.WriteLine($"El vehículo con matrícula: {Matricula} {_etiqueta} tiene que pagar como impuesto {cantidadImpuesto} Euros");
+
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ PracticaCochesSolucion/PracticaCoches/Program.cs
@@ -0,0 +1,40 @@
+﻿using Entidades;
+
+
+namespace PracticaCoches
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Crear los Vehiculos
+            var coche1 = new Vehiculos("1234HRH", new DateTime(2022, 01, 15), Vehiculos.Etiqueta.sinEtiqueta);
+
+            var coche2 = new Vehiculos("5678ARS", new DateTime(2020, 04, 05), Vehiculos.Etiqueta.etiquetaB);
+
+            var coche3 = new Vehiculos("1268FRS", new DateTime(1990, 08, 15), Vehiculos.Etiqueta.etiquetaC);
+
+            var coche4 = new Vehiculos("3265JHI", new DateTime(1998, 06, 20), Vehiculos.Etiqueta.etiquetaCERO);
+
+            var coche5 = new Vehiculos("4573FFM", new DateTime(2010, 03, 30), Vehiculos.Etiqueta.etiquetaECO);
+
+
+
+            var listaCoches = new List<Vehiculos>();
+            listaCoches.Add(coche1);
+            listaCoches.Add(coche2);
+            listaCoches.Add(coche3);
+            listaCoches.Add(coche4);
+            listaCoches.Add(coche5);
+
+            foreach (var vehiculo in listaCoches)
+            {
+                vehiculo.CalcularImpuesto();
+            }
+
+
+
+        }
+    }
+}
