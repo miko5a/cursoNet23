14ef99510668105d42f0a3fed3194ad5818027ca
Alberto de León <alberto13590@hotmail.com>
2023-07-19 10:00:45 +0200

    Commit Nueva Practica

---
+++ PracticaVehiculosSolucion/PracticaVehiculos/Program.cs
@@ -0,0 +1,26 @@
+﻿using Entidades;
+
+namespace PracticaVehiculos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear los Vehiculos
+            var coche1 = new Vehiculos(new DateTime(2012, 12, 12), "Rojo");
+            var coche2 = new Vehiculos(new DateTime(2002, 12, 12), "Seat", "Ibiza", "Verde");
+            var coche3 = new Vehiculos("Seat", "Toledo");
+
+            var listaCoches = new List<Vehiculos>();
+            listaCoches.Add(coche1);
+            listaCoches.Add(coche2);
+            listaCoches.Add(coche3);
+
+            foreach (var vehiculo in listaCoches)
+            {
+                vehiculo.MostrarVehiculo();
+            }
+
+        }
+    }
+}
---
+++ PracticaVehiculosSolucion/Vehiculos/Vehiculos.cs
@@ -0,0 +1,48 @@
+﻿namespace Entidades
+{
+    public class Vehiculos
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public DateTime AñoCompra { get; set; }
+
+
+        public Vehiculos(DateTime añoCompra, string color)
+        {
+            AñoCompra = añoCompra;
+            Color = color;
+        }
+        public Vehiculos(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
+        public Vehiculos(DateTime añoCompra, string marca, string modelo, string color)
+        {
+            AñoCompra = añoCompra;
+            Marca = marca;
+            Modelo = modelo;
+            Color = color;
+        }
+
+        public void MostrarVehiculo()
+        {
+            Console.WriteLine($"=========   Vehículo   =========");
+            if (AñoCompra.Year != 1) { 
+                Console.WriteLine($"Año Compra: {AñoCompra.Year}");
+            }
+            if (Marca != null){
+                Console.WriteLine($"Marca: {Marca}");
+            }
+            if (Modelo != null)
+            {
+                Console.WriteLine($"Modelo: {Modelo}");
+            }
+            if (Color != null)
+            {
+                Console.WriteLine($"Color: {Color}");
+            }
+        }
+    }
+}
