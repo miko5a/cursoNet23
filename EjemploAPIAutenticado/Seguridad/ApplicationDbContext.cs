6e104dd2295bdf10aacfbb6f0fff910b3df2fd19
Alberto de León <alberto13590@hotmail.com>
2023-08-25 13:25:43 +0200

    Update

---
+++ EjemploAPIAutenticado/Seguridad/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EjemploAPIAutenticado.Seguridad;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EjemploAPIAutenticado.Auth
+{
+    public class ApplicationDbContext : IdentityDbContext<Usuario>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ EjemploAPIAutenticado/Seguridad/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EjemploAPIAutenticado.Seguridad
+{
+    public class Usuario : IdentityUser
+    {
+        public int IdPais { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+
+    public class Pais
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
