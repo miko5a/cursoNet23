77079523f8a2e14e8b48796512e93beddcba5477
Alberto de León <alberto13590@hotmail.com>
2023-09-14 08:18:33 +0200

    prueba repositorio

---
+++ ExamenSummerCamp/ExamenSummerCampSolucion/Modelos/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+    }
+}
